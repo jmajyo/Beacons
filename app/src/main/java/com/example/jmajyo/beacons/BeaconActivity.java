8851999e7ebc89acefb890b1bd651f388861817d
jmajyo-por <jmajyo@gmail.com>
2017-02-27 13:55:01 +0100

    added bluetooth permision, now it´s sho in screen the distance.

--- app/src/main/java/com/example/jmajyo/beacons/BeaconActivity.java
+++ app/src/main/java/com/example/jmajyo/beacons/BeaconActivity.java
@@ -4,6 +4,7 @@ import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -20,11 +21,16 @@ public class BeaconActivity extends AppCompatActivity implements BeaconConsumer 
 
     public static final String TAG = "BeaconsEverywhere";
     private BeaconManager beaconManager;
+    private TextView text;
+    private Beacon beacon;
+    private boolean beaconbool=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beacon);
+
+        text = (TextView) findViewById(R.id.activity_beacon___text);
 
         beaconManager = BeaconManager.getInstanceForApplication(this);
 
@@ -33,7 +39,10 @@ public class BeaconActivity extends AppCompatActivity implements BeaconConsumer 
 
         beaconManager.bind(this);
 
-        }
+       // if(beaconbool ==true)
+           // text.setText("Distancia: " + beacon.getDistance());
+
+    }
 
     @Override
     protected void onDestroy() {
@@ -43,7 +52,7 @@ public class BeaconActivity extends AppCompatActivity implements BeaconConsumer 
 
     @Override
     public void onBeaconServiceConnect() {
-        final Region region = new Region("myBeaons", Identifier.parse("699ebc80-e1f3-11e3-9a0f-0cf3ee3bc012"), null, null);
+        final Region region = new Region("myBeacons", Identifier.parse("699ebc80-e1f3-11e3-9a0f-0cf3ee3bc012"), null, null);
 
         beaconManager.setMonitorNotifier(new MonitorNotifier() {
             @Override
@@ -75,8 +84,16 @@ public class BeaconActivity extends AppCompatActivity implements BeaconConsumer 
         beaconManager.setRangeNotifier(new RangeNotifier() {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                for(Beacon oneBeacon : beacons) {
+                for(final Beacon oneBeacon : beacons) {
                     Log.d(TAG, "distance: " + oneBeacon.getDistance() + " id:" + oneBeacon.getId1() + "/" + oneBeacon.getId2() + "/" + oneBeacon.getId3());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(!oneBeacon.getId3().equals("34782")) {
+                                text.setText(String.format("Distancia: %.02g", oneBeacon.getDistance()));
+                            }
+                        }
+                    });
                 }
             }
         });
